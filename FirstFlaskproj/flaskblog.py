7d433ccd5ac38bea237d9b3af3ca6b4e4ef02336
Atul2020 <49779413+Atul2020@users.noreply.github.com>
2020-10-31 19:16:06 +0530

    created form

--- FirstFlaskproj/flaskblog.py
+++ FirstFlaskproj/flaskblog.py
@@ -1,6 +1,7 @@
 from flask import Flask,render_template,url_for
+from forms import RegistrationForm,loginForm
 app= Flask(__name__)
-
+  app.config['SECRET_KEY']='5435rtee45ww0495jtre49tjpbmnt'
 posts=[
     {
     'Book_Name':'Diary of a Wimpy Kid',
@@ -23,6 +24,20 @@ def hello():
 @app.route("/about")
 def about():
     return render_template('about.html',title='About')
+
+@app.route("/register")
+def register():
+    form=RegistrationForm()
+    return render_template('register.html',title='Register',form=form)
+
+
+@app.route("/login")
+def login():
+    form=loginForm()
+    return render_template('login.html',title='Login',form=form)
+
+
+
 
 if __name__=='__main__':
     app.run(debug=True)
